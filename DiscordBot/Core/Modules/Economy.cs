a30909b21104a0479445990f689e282e3114648f
Greem Husted <gr33m11@gmail.com>
2018-03-07 19:08:37 -0500

    merged all leveling commands into the economy module and delete the Leveling.cs

--- DiscordBot/Core/Modules/Economy.cs
+++ DiscordBot/Core/Modules/Economy.cs
@@ -3,11 +3,45 @@ using System.Threading.Tasks;
 using SIVA.Core.UserAccounts;
 using Discord;
 using Discord.WebSocket;
+using System;
 
 namespace SIVA.Core.Modules
 {
     public class Economy : ModuleBase<SocketCommandContext>
     {
+        [Command("WhatLevelIs"), Alias("WLI")]
+        public async Task WhatLevelIs(uint xp)
+        {
+            uint level = (uint)Math.Sqrt(xp / 50);
+            await ReplyAsync("The level is " + level);
+        }
+
+        [Command("Level"), Priority(0)]
+        public async Task Level()
+        {
+            var ua = UserAccounts.UserAccounts.GetAccount(Context.User);
+            var embed = new EmbedBuilder();
+            embed.WithTitle("User Level");
+            embed.WithDescription(Utilities.GetFormattedLocaleMsg("LevelCommandText", Context.User.Mention, ua.LevelNumber));
+            embed.WithFooter(Utilities.GetFormattedLocaleMsg("CommandFooter", Context.User.Username));
+            embed.WithColor(SIVA.Config.bot.DefaultEmbedColour);
+
+            await ReplyAsync("", false, embed);
+        }
+
+        [Command("Level"), Priority(1)]
+        public async Task Level(SocketGuildUser user)
+        {
+            var ua = UserAccounts.UserAccounts.GetAccount(user);
+            var embed = new EmbedBuilder();
+            embed.WithTitle("User Level");
+            embed.WithDescription(Utilities.GetFormattedLocaleMsg("LevelCommandText", user.Mention, ua.LevelNumber));
+            embed.WithFooter(Utilities.GetFormattedLocaleMsg("CommandFooter", Context.User.Username));
+            embed.WithColor(SIVA.Config.bot.DefaultEmbedColour);
+
+            await ReplyAsync("", false, embed);
+        }
+
         [Command("Money"), Alias("$", "bal")]
         public async Task HowMuchDoIHave()
         {
