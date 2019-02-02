cd159d843d170d0cae1aa68688eb29fcbd3160f9
Michael Husted <gr33m11@gmail.com>
2019-02-02 15:51:52 -0600

    fixes #5

--- Volte/Core/Modules/Utility/InviteCommand.cs
+++ Volte/Core/Modules/Utility/InviteCommand.cs
@@ -12,7 +12,7 @@ namespace Volte.Core.Modules.Utility {
             await Reply(Context.Channel,
                 CreateEmbed(Context,
                     "Do you like Volte? If you do, that's awesome! If not then I'm sorry :( \n\n" +
-                    "[Website](https://volte.greemdev.net)" +
+                    "[Website](https://volte.greemdev.net)\n" +
                     $"[Invite Me](https://discordapp.com/oauth2/authorize?client_id={VolteBot.Client.CurrentUser.Id}&scope=bot&permissions=8)\n" +
                     "[Support Server Invite](https://greemdev.net/discord)\n\n" +
                     "And again, thanks for using me!"));
