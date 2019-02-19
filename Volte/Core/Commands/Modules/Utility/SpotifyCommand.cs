0894857e30df22d4f7824c738afdcb4434a28878
Michael Husted <gr33m11@gmail.com>
2019-02-18 23:03:19 -0600

    Show what you're listening to via the new Spotify command.

---
+++ Volte/Core/Commands/Modules/Utility/SpotifyCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Humanizer;
+using Qmmands;
+using Volte.Core.Extensions;
+
+namespace Volte.Core.Commands.Modules.Utility
+{
+    public partial class UtilityModule : VolteModule
+    {
+        [Command("Spotify")]
+        [Description("Shows what you're listening to on Spotify, if you're listening to something.")]
+        [Remarks("Usage: |prefix|spotify")]
+        public async Task Spotify()
+        {
+            if (Context.User.Activity is SpotifyGame spotify)
+            {
+                var e = Context.CreateEmbedBuilder(string.Empty)
+                    .WithDescription($"**Track:** [{spotify.TrackTitle}]({spotify.TrackUrl})\n" +
+                                     $"**Album:** {spotify.AlbumTitle}\n" +
+                                     $"**Duration:** {spotify.Duration}\n" +
+                                     $"**Artists:** {string.Join(',', spotify.Artists)}")
+                    .WithThumbnailUrl(spotify.AlbumArtUrl)
+                    .SendTo(Context.Channel);
+                return;
+
+            }
+            await Context.CreateEmbed("You're not listening to Spotify right now.").SendTo(Context.Channel);
+        }
+    }
+}
