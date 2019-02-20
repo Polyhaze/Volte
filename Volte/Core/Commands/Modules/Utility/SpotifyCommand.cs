ea98dcbd9e4ace79cfdf9e531f7241cad7c74a27
Michael Husted <gr33m11@gmail.com>
2019-02-19 19:02:56 -0600

    spotify command imports fixed

--- Volte/Core/Commands/Modules/Utility/SpotifyCommand.cs
+++ Volte/Core/Commands/Modules/Utility/SpotifyCommand.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Discord;
-using Humanizer;
 using Qmmands;
 using Volte.Core.Extensions;
 
