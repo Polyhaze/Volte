7d0432801282823e10aaf4efc6cbb88dbde6af5c
Michael Husted <gr33m11@gmail.com>
2019-02-23 18:12:14 -0600

    GitHubService; for the release after V2.2's git interaction commands.

---
+++ Volte/Core/Services/GitHubService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace Volte.Core.Services
+{
+    public class GitHubService : IService
+    {
+        public readonly GitHubClient GitHub = new GitHubClient(new ProductHeaderValue("GreemDev"));
+
+        public async Task<Commit> GetLastCommit()
+        {
+            return (await GitHub.Repository.Commit.GetAll("GreemDev", "Volte"))
+                .Select(x => x.Commit).Take(1).ElementAt(0);
+        }
+
+        public async Task<IEnumerable<Commit>> GetAllCommits()
+        {
+            return (await GitHub.Repository.Commit.GetAll("GreemDev", "Volte")).Select(x => x.Commit);
+        }
+
+        public async Task<int> GetOpenIssuesCount()
+        {
+            return (await GitHub.Repository.Get("GreemDev", "Volte")).OpenIssuesCount;
+        }
+
+        public async Task<IReadOnlyList<PullRequest>> GetOpenPullRequests()
+        {
+            return await GitHub.PullRequest.GetAllForRepository("GreemDev", "Volte");
+        }
+    }
+}
