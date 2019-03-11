efe1645c4d385add687c8afa589683ea29ce4dca
Michael Husted <gr33m11@gmail.com>
2019-03-10 23:26:51 -0500

    config command: show your entire server config in JSON

---
+++ src/Commands/Modules/Admin/ConfigCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Qmmands;
+using Volte.Commands.Preconditions;
+using Volte.Extensions;
+
+namespace Volte.Commands.Modules.Admin
+{
+    public partial class AdminModule : VolteModule
+    {
+        [Command("Config")]
+        [Description("Shows a JSON-formatted version of the object for detailed inspection.")]
+        [Remarks("Usage: |prefix|config")]
+        [RequireGuildAdmin]
+        public async Task ConfigAsync()
+        {
+            await Context.CreateEmbed(DebugService.Execute(Db.GetConfig(Context.Guild).ToString()))
+                .SendToAsync(Context.Channel);
+        }
+    }
+}
