9b34dd9822931fd3ba70f655e1d9b395ad5ac429
Michael Husted <gr33m11@gmail.com>
2019-03-22 23:13:49 -0500

    extensions should be faster:tm: (don't quote me on that)

--- src/Extensions/CollectionExtensions.cs
+++ src/Extensions/CollectionExtensions.cs
@@ -7,12 +7,8 @@ namespace Volte.Extensions
 {
     public static class CollectionExtensions
     {
-        public static Stream ToStream(this IEnumerable<byte> bytes)
-        {
-            var memStream = new MemoryStream(bytes.Cast<byte[]>() ?? bytes.ToArray(), false);
-            memStream.Seek(0, SeekOrigin.Begin);
-            return memStream;
-        }
+        public static Stream ToStream(this IEnumerable<byte> bytes) =>
+            new MemoryStream(bytes.Cast<byte[]>() ?? bytes.ToArray(), false) {Position = 0};
 
         public static bool ContainsIgnoreCase(this IEnumerable<string> strings, string element) =>
             strings.Contains(element, StringComparer.OrdinalIgnoreCase);
--- src/Extensions/HttpExtensions.cs
+++ src/Extensions/HttpExtensions.cs
@@ -4,12 +4,7 @@ namespace Volte.Extensions
 {
     public static class HttpExtensions
     {
-        public static bool IsImage(this HttpResponseMessage msg)
-        {
-            var mime = msg.Content.Headers.ContentType.MediaType;
-            return mime.EqualsIgnoreCase("image/png")
-                   || mime.EqualsIgnoreCase("image/jpeg")
-                   || mime.EqualsIgnoreCase("image/gif");
-        }
+        public static bool IsImage(this HttpResponseMessage msg) =>
+            msg.Content.Headers.ContentType.MediaType.StartsWith("image/");
     }
 }
