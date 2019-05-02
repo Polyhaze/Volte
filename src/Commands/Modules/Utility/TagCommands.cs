a7370d47454595c1d4d871a1769a40cef32e4d6a
Michael Husted <gr33m11@gmail.com>
2019-05-02 01:02:54 -0500

    Tags command: list all available tags in the current guild

--- src/Commands/Modules/Utility/TagCommands.cs
+++ src/Commands/Modules/Utility/TagCommands.cs
@@ -42,7 +42,7 @@ namespace Volte.Commands.Modules.Utility
         [Priority(1)]
         [Description("Shows stats for a tag.")]
         [Remarks("Usage: |prefix|tagstats {name}")]
-        public async Task TagStats([Remainder] string name)
+        public async Task TagStatsAsync([Remainder] string name)
         {
             var config = Db.GetConfig(Context.Guild);
             var tag = config.Tags.FirstOrDefault(t => t.Name.EqualsIgnoreCase(name));
@@ -59,9 +59,24 @@ namespace Volte.Commands.Modules.Utility
             await Context.CreateEmbed(string.Empty).ToEmbedBuilder()
                 .WithTitle($"Tag {tag.Name}")
                 .AddField("Response", $"`{tag.Response}`", true)
-                .AddField("Creator", u == null ? $"{tag.CreatorId}" : $"{u.Mention}", true)
+                .AddField("Creator", u is null ? $"{tag.CreatorId}" : $"{u.Mention}", true)
                 .AddField("Uses", $"**{tag.Uses}**", true)
                 .SendToAsync(Context.Channel);
         }
+
+        [Command("Tags")]
+        [Description("Lists all available tags in the current guild.")]
+        [Remarks("Usage: |prefix|tags")]
+        public async Task TagsAsync()
+        {
+            var config = Db.GetConfig(Context.Guild);
+            await Context.CreateEmbedBuilder(
+                config.Tags.Count == 0 
+                ? "None" 
+                : $"`{config.Tags.Select(x => x.Name).Join("`, `")}`"
+                ).WithTitle($"Available Tags for {Context.Guild.Name}")
+                .SendToAsync(Context.Channel);
+        }
+
     }
 }
