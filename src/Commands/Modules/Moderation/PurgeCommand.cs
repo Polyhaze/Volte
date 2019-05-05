b7d1a8136f8d209145546a50e50ccb4d4baba3b5
Michael Husted <gr33m11@gmail.com>
2019-05-04 23:53:18 -0500

    Ability to purge by certain user.

--- src/Commands/Modules/Moderation/PurgeCommand.cs
+++ src/Commands/Modules/Moderation/PurgeCommand.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
+using Discord.WebSocket;
 using Qmmands;
 using Volte.Commands.Preconditions;
 using Volte.Data.Objects;
@@ -12,19 +14,24 @@ namespace Volte.Commands.Modules.Moderation
     public partial class ModerationModule : VolteModule
     {
         [Command("Purge", "clear", "clean")]
-        [Description("Purges the last x messages.")]
-        [Remarks("Usage: $purge {count}")]
+        [Description("Purges the last x messages, or the last x messages by a given user.")]
+        [Remarks("Usage: $purge {count} [targetAuthor]")]
         [RequireBotChannelPermission(ChannelPermission.ManageMessages)]
         [RequireGuildModerator]
-        public async Task PurgeAsync(int count)
+        public async Task PurgeAsync(int count, SocketGuildUser targetAuthor = null)
         {
-            await Context.Channel.DeleteMessagesAsync(await Context.Channel.GetMessagesAsync(count + 1).FlattenAsync());
+            var messages = await Context.Channel.GetMessagesAsync(count + 1).FlattenAsync();
+            if (!(targetAuthor is null))
+                await Context.Channel.DeleteMessagesAsync(messages.Where(x => x.Author.Id == targetAuthor.Id));
+            else
+                await Context.Channel.DeleteMessagesAsync(messages);
+
             //+1 to include the command invocation message, and actually delete the last x messages instead of x - 1.
             //lets you theoretically use 0 to delete only the invocation message, for testing or something.
             var msg = await Context
-                .CreateEmbed($"Successfully deleted **{count}** message{(count.ShouldBePlural() ? "s" : string.Empty)}")
+                .CreateEmbed($"Successfully deleted **{messages.Count()}** message{(messages.Count().ShouldBePlural() ? "s" : string.Empty)}")
                 .SendToAsync(Context.Channel);
-            await Executor.ExecuteAfterDelayAsync(3000, async () => await msg.DeleteAsync());
+            _ = Executor.ExecuteAfterDelayAsync(3000, async () => await msg.DeleteAsync());
             await EventService.OnModActionCompleteAsync(new ModActionEventArgs(Context, ModActionType.Purge, count));
         }
     }
