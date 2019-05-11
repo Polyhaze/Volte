d0d14d01e8e22d0cd0a5e7b2f77041f279074b18
Michael Husted <gr33m11@gmail.com>
2019-05-11 01:09:49 -0500

    version bumped and release type changed

--- src/Version.cs
+++ src/Version.cs
@@ -5,10 +5,10 @@ namespace Volte
     public static class Version
     {
         internal static int Major { get; } = 2;
-        internal static int Minor { get; } = 3;
-        internal static int Patch { get; } = 1;
+        internal static int Minor { get; } = 4;
+        internal static int Patch { get; } = 0;
         internal static int Hotfix { get; } = 0;
-        public static ReleaseType ReleaseType { get; } = ReleaseType.Development;
+        public static ReleaseType ReleaseType { get; } = ReleaseType.Release;
         public static string FullVersion { get; } = $"{Major}.{Minor}.{Patch}.{Hotfix}-{ReleaseType}";
         public static string DiscordNetVersion { get; } = DiscordConfig.Version;
     }
