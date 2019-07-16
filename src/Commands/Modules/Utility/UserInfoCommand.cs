76b62a4d64362a91bc27b67614e8de17cce60130
Michael Husted <gr33m11@gmail.com>
2019-07-15 22:34:45 -0500

    🦀🦀🦀 userinfo nullref is gone 🦀🦀🦀

--- src/Commands/Modules/Utility/UserInfoCommand.cs
+++ src/Commands/Modules/Utility/UserInfoCommand.cs
@@ -21,7 +21,7 @@ namespace Volte.Commands.Modules.Utility
                 .WithThumbnailUrl(target.GetAvatarUrl())
                 .WithTitle("User Info")
                 .AddField("User ID", target.Id, true)
-                .AddField("Game", target.Activity.Name ?? "Nothing", true)
+                .AddField("Game", target.Activity?.Name ?? "Nothing", true)
                 .AddField("Status", target.Status, true)
                 .AddField("Is Bot", target.IsBot, true)
                 .AddField("Account Created",
