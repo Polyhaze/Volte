742c38b350acd9532b4e97ae545164dd2fb72a33
Michael Husted <gr33m11@gmail.com>
2019-07-15 20:14:20 -0500

    touch up Purge command

--- src/Commands/Modules/Moderation/PurgeCommand.cs
+++ src/Commands/Modules/Moderation/PurgeCommand.cs
@@ -20,16 +20,19 @@ namespace Volte.Commands.Modules.Moderation
         [RequireGuildModerator]
         public async Task PurgeAsync(int count, SocketGuildUser targetAuthor = null)
         {
+            //+1 to include the command invocation message, and actually delete the last x messages instead of x - 1.
+            //lets you theoretically use 0 to delete only the invocation message, for testing or something.
             var messages = await Context.Channel.GetMessagesAsync(count + 1).FlattenAsync();
             if (!(targetAuthor is null))
                 await Context.Channel.DeleteMessagesAsync(messages.Where(x => x.Author.Id == targetAuthor.Id));
             else
                 await Context.Channel.DeleteMessagesAsync(messages);
 
-            //+1 to include the command invocation message, and actually delete the last x messages instead of x - 1.
-            //lets you theoretically use 0 to delete only the invocation message, for testing or something.
+            //-1 to show that the correct amount of messages were deleted.
+            var mCount = messages.Count() - 1;
             var msg = await Context
-                .CreateEmbed($"Successfully deleted **{messages.Count()}** message{(messages.Count().ShouldBePlural() ? "s" : string.Empty)}")
+                .CreateEmbed($"Successfully deleted **{mCount}** " +
+                             $"message{(mCount.ShouldBePlural() ? "s" : string.Empty)}")
                 .SendToAsync(Context.Channel);
             _ = Executor.ExecuteAfterDelayAsync(3000, async () => await msg.DeleteAsync());
             await ModLogService.OnModActionCompleteAsync(new ModActionEventArgs(Context, ModActionType.Purge, count));
