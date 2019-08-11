94c9c09a85eafb140676352cadf85e5cacf61bfa
Leah Husted <gr33m11@gmail.com>
2019-08-11 00:49:15 -0500

    eval helper methods

--- src/Core/Models/EvalObjects.cs
+++ src/Core/Models/EvalObjects.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Linq;
 using Discord.WebSocket;
+using Gommon;
 using Qmmands;
 using Volte.Commands;
 using Volte.Core.Models.Guild;
@@ -17,5 +20,24 @@ namespace Volte.Core.Models
         public CommandService CommandService { get; set; }
         public DatabaseService DatabaseService { get; set; }
         public EmojiService EmojiService { get; set; }
+
+        public SocketGuildUser User(ulong id) 
+            => Context.Guild.GetUser(id);
+
+        public SocketGuildUser User(string username) 
+            => Context.Guild.Users.FirstOrDefault(a => a.Username.EqualsIgnoreCase(username) || (a.Nickname != null && a.Nickname.EqualsIgnoreCase(username)));
+
+        public SocketTextChannel TextChannel(ulong id) 
+            => Context.Guild.GetTextChannel(id);
+
+        public SocketTextChannel TextChannel(string name) 
+            => Context.Guild.TextChannels.FirstOrDefault(a => a.Name.EqualsIgnoreCase(name));
+
+        public SocketUserMessage Message(ulong id) 
+            => Context.Channel.GetCachedMessage(id) as SocketUserMessage;
+
+        public SocketUserMessage Message(string id) 
+            => Message(ulong.Parse(id));
+
     }
 }
