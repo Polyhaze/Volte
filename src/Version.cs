ca27cd5f2920c021a1d9e2bd81faa0633319c0cb
Leah Husted <gr33m11@gmail.com>
2019-08-13 01:13:54 -0500

    bump version. release notes coming soon

--- src/Version.cs
+++ src/Version.cs
@@ -6,7 +6,7 @@
         private static uint Minor => 0;
         private static uint Patch => 2;
         private static uint Hotfix => 0;
-        public static ReleaseType ReleaseType => ReleaseType.Development;
+        public static ReleaseType ReleaseType => ReleaseType.Release;
         public static string FullVersion => $"{Major}.{Minor}.{Patch}.{Hotfix}-{ReleaseType}";
         public static string DiscordNetVersion => Discord.DiscordConfig.Version;
     }
