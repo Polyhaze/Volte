150e14ad379682f29f925ebae44f6c30239ca723
Leah Husted <gr33m11@gmail.com>
2019-09-23 23:32:15 -0500

    bump version

--- src/Version.cs
+++ src/Version.cs
@@ -5,9 +5,9 @@
         public static System.Version AsDotNetVersion => new System.Version(Major, Minor, Patch, Hotfix);
         private static int Major => 3;
         private static int Minor => 0;
-        private static int Patch => 2;
-        private static int Hotfix => 1;
-        public static ReleaseType ReleaseType => ReleaseType.Development;
+        private static int Patch => 3;
+        private static int Hotfix => 0;
+        public static ReleaseType ReleaseType => ReleaseType.Release;
         public static string FullVersion => $"{Major}.{Minor}.{Patch}.{Hotfix}-{ReleaseType}";
         public static string DiscordNetVersion => Discord.DiscordConfig.Version;
     }
