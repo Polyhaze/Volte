f089cad570337b47abb25e3ccf6efb7d3a00e4da
Leah Husted <gr33m11@gmail.com>
2019-10-10 01:25:38 -0500

    my dnet fork is broken, revert to mainline dnet for now
    also change the poll command to not have all the ugly in the command file, and continue when it can't delete the message

--- src/Commands/Modules/Utility/PollCommand.cs
+++ src/Commands/Modules/Utility/PollCommand.cs
@@ -1,8 +1,9 @@
-﻿using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Discord;
+using Gommon;
 using Qmmands;
 using Volte.Commands.Results;
+using Volte.Helpers;
 
 namespace Volte.Commands.Modules
 {
@@ -16,127 +17,14 @@ namespace Volte.Commands.Modules
             var question = pollText.Split(';')[0];
             var choices = pollText.Split(';');
 
-            string embedBody;
-            var sb = new StringBuilder();
-
-            switch (choices.Length - 1)
-            {
-                case 1:
-                {
-                    embedBody = sb.AppendLine($"{new Emoji(EmojiService.One)} {choices[1]}")
-                        .AppendLine()
-                        .AppendLine("Click the number below to vote.")
-                        .ToString();
-                    break;
-                }
-
-                case 2:
-                {
-                    embedBody = sb.AppendLine($"{new Emoji(EmojiService.One)} {choices[1]}")
-                        .AppendLine($"{new Emoji(EmojiService.Two)} {choices[2]}")
-                        .AppendLine()
-                        .AppendLine("Click the number below to vote.")
-                        .ToString();
-                    break;
-                }
-
-                case 3:
-                {
-                    embedBody = sb.AppendLine($"{new Emoji(EmojiService.One)} {choices[1]}")
-                        .AppendLine($"{new Emoji(EmojiService.Two)} {choices[2]}")
-                        .AppendLine($"{new Emoji(EmojiService.Three)} {choices[3]}")
-                        .AppendLine()
-                        .AppendLine("Click the number below to vote.")
-                        .ToString();
-                    break;
-                }
-
-                case 4:
-                {
-                    embedBody = sb.AppendLine($"{new Emoji(EmojiService.One)} {choices[1]}")
-                        .AppendLine($"{new Emoji(EmojiService.Two)} {choices[2]}")
-                        .AppendLine($"{new Emoji(EmojiService.Three)} {choices[3]}")
-                        .AppendLine($"{new Emoji(EmojiService.Three)} {choices[4]}")
-                        .AppendLine()
-                        .AppendLine("Click the number below to vote.")
-                        .ToString();
-                    break;
-                }
-
-                case 5:
-                    embedBody = sb.AppendLine($"{new Emoji(EmojiService.One)} {choices[1]}")
-                        .AppendLine($"{new Emoji(EmojiService.Two)} {choices[2]}")
-                        .AppendLine($"{new Emoji(EmojiService.Three)} {choices[3]}")
-                        .AppendLine($"{new Emoji(EmojiService.Three)} {choices[4]}")
-                        .AppendLine($"{new Emoji(EmojiService.Three)} {choices[5]}")
-                        .AppendLine()
-                        .AppendLine("Click the number below to vote.")
-                        .ToString();
-                    break;
-
-                default:
-                {
-                    if (choices.Length - 1 > 5)
-                    {
-                        embedBody = "More than 5 options were specified.";
-                        break;
-                    }
-
-                    embedBody = "No options specified.";
-                    break;
-                }
-            }
-
-
             return Ok(Context.CreateEmbedBuilder()
                     .WithTitle(question)
                     .WithThumbnailUrl("http://survation.com/wp-content/uploads/2016/09/polleverywherelogo.png")
-                    .WithDescription(embedBody),
+                    .WithDescription(PollHelpers.GetPollBody(choices, EmojiService)),
                 async msg =>
                 {
-                    await Context.Message.DeleteAsync();
-                    switch (choices.Length - 1)
-                    {
-                        case 1:
-                        {
-                            await msg.AddReactionAsync(new Emoji(EmojiService.One));
-                            break;
-                        }
-
-                        case 2:
-                        {
-                            await msg.AddReactionAsync(new Emoji(EmojiService.One));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Two));
-                            break;
-                        }
-
-                        case 3:
-                        {
-                            await msg.AddReactionAsync(new Emoji(EmojiService.One));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Two));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Three));
-                            break;
-                        }
-
-                        case 4:
-                        {
-                            await msg.AddReactionAsync(new Emoji(EmojiService.One));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Two));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Three));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Four));
-                            break;
-                        }
-
-                        case 5:
-                        {
-                            await msg.AddReactionAsync(new Emoji(EmojiService.One));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Two));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Three));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Four));
-                            await msg.AddReactionAsync(new Emoji(EmojiService.Five));
-                            break;
-                        }
-                    }
+                    _ = await Context.Message.TryDeleteAsync();
+                    await PollHelpers.AddPollReactionsAsync(choices, msg, EmojiService);
                 });
         }
     }
---
+++ src/Helpers/PollHelpers.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Volte.Services;
+
+namespace Volte.Helpers
+{
+    public static class PollHelpers
+    {
+        public static string GetPollBody(string[] choices, EmojiService emojiService)
+        {
+            var sb = new StringBuilder();
+            switch (choices.Length - 1)
+            {
+                case 1:
+                {
+                    return sb.AppendLine($"{new Emoji(emojiService.One)} {choices[1]}")
+                        .AppendLine()
+                        .AppendLine("Click the number below to vote.")
+                        .ToString();
+                }
+
+                case 2:
+                {
+                    return sb.AppendLine($"{new Emoji(emojiService.One)} {choices[1]}")
+                        .AppendLine($"{new Emoji(emojiService.Two)} {choices[2]}")
+                        .AppendLine()
+                        .AppendLine("Click the number below to vote.")
+                        .ToString();
+                }
+
+                case 3:
+                {
+                    return sb.AppendLine($"{new Emoji(emojiService.One)} {choices[1]}")
+                        .AppendLine($"{new Emoji(emojiService.Two)} {choices[2]}")
+                        .AppendLine($"{new Emoji(emojiService.Three)} {choices[3]}")
+                        .AppendLine()
+                        .AppendLine("Click the number below to vote.")
+                        .ToString();
+                }
+
+                case 4:
+                {
+                    return sb.AppendLine($"{new Emoji(emojiService.One)} {choices[1]}")
+                        .AppendLine($"{new Emoji(emojiService.Two)} {choices[2]}")
+                        .AppendLine($"{new Emoji(emojiService.Three)} {choices[3]}")
+                        .AppendLine($"{new Emoji(emojiService.Three)} {choices[4]}")
+                        .AppendLine()
+                        .AppendLine("Click the number below to vote.")
+                        .ToString();
+                }
+
+                case 5:
+                    return sb.AppendLine($"{new Emoji(emojiService.One)} {choices[1]}")
+                        .AppendLine($"{new Emoji(emojiService.Two)} {choices[2]}")
+                        .AppendLine($"{new Emoji(emojiService.Three)} {choices[3]}")
+                        .AppendLine($"{new Emoji(emojiService.Three)} {choices[4]}")
+                        .AppendLine($"{new Emoji(emojiService.Three)} {choices[5]}")
+                        .AppendLine()
+                        .AppendLine("Click the number below to vote.")
+                        .ToString();
+
+                default:
+                {
+                    return choices.Length - 1 > 5 ? "More than 5 options were specified." : "No options specified.";
+                }
+            }
+        }
+
+        public static async Task AddPollReactionsAsync(string[] choices, IUserMessage msg,EmojiService EmojiService)
+        {
+            switch (choices.Length - 1)
+            {
+                case 1:
+                {
+                    await msg.AddReactionAsync(new Emoji(EmojiService.One));
+                    break;
+                }
+
+                case 2:
+                {
+                    await msg.AddReactionAsync(new Emoji(EmojiService.One));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Two));
+                    break;
+                }
+
+                case 3:
+                {
+                    await msg.AddReactionAsync(new Emoji(EmojiService.One));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Two));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Three));
+                    break;
+                }
+
+                case 4:
+                {
+                    await msg.AddReactionAsync(new Emoji(EmojiService.One));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Two));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Three));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Four));
+                    break;
+                }
+
+                case 5:
+                {
+                    await msg.AddReactionAsync(new Emoji(EmojiService.One));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Two));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Three));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Four));
+                    await msg.AddReactionAsync(new Emoji(EmojiService.Five));
+                    break;
+                }
+            }
+        }
+    }
+}
