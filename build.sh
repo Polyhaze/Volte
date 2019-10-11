75d1a3e8967862a5b682e2b1e328be5217fa9c02
Leah Husted <gr33m11@gmail.com>
2019-10-11 03:11:57 -0500

    use publishtrimmed for a smaller executable (about 30MB less in the executable!)

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-dotnet restore
-dotnet publish -c release -r linux-x64 -p:PublishSingleFile=true && dotnet publish -c release -r win10-x64 -p:PublishSingleFile=true
+dotnet publish -c release -r linux-x64 && dotnet publish -c release -r win10-x64
