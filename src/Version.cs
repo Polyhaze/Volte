dd48267b5881eb3ce5db1fdd2df57a4ed3a763a5
Leah Husted <gr33m11@gmail.com>
2019-10-16 01:39:52 -0500

    change hotfix ver

--- src/Version.cs
+++ src/Version.cs
@@ -6,7 +6,7 @@
         private static int Major => 3;
         private static int Minor => 1;
         private static int Patch => 0;
-        private static int Hotfix => 0;
+        private static int Hotfix => 1;
         public static ReleaseType ReleaseType => ReleaseType.Release;
         public static string FullVersion => $"{Major}.{Minor}.{Patch}.{Hotfix}-{ReleaseType}";
         public static string DiscordNetVersion => Discord.DiscordConfig.Version;
