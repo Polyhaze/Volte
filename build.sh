230cc6684c66b6d7ffc810bd1478f67436eb4a1d
Leah Husted <gr33m11@gmail.com>
2019-10-16 01:56:44 -0500

    properly make the GitHub Actions script generate trimmed, single-file binaries

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-dotnet publish -c release -r linux-x64 && dotnet publish -c release -r win10-x64
+dotnet publish -c release -r linux-x64 -p:PublishSingleFile=true && dotnet publish -c release -r win10-x64 -p:PublishSingleFile=true
