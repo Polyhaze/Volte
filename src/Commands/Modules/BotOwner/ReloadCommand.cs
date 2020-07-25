aec6942b25c9a6b05937ac1c96696ab97248e7cb
BarisYildiz <barisyil97@gmail.com>
2020-07-25 02:35:28 +0200

    fixed crash when log_all_commands was set to true (#41)

--- src/Commands/Modules/BotOwner/ReloadCommand.cs
+++ src/Commands/Modules/BotOwner/ReloadCommand.cs
@@ -2,6 +2,7 @@
 using Qmmands;
 using Volte.Core.Attributes;
 using Volte.Core;
+using Volte.Commands.Results;
 
 namespace Volte.Commands.Modules
 {
@@ -12,7 +13,7 @@ namespace Volte.Commands.Modules
             "Reloads the bot's configuration file if you've changed it. NOTE: This will throw an exception if the config file is invalid JSON!")]
         [Remarks("reload")]
         [RequireBotOwner]
-        public Task ReloadAsync()
+        public Task<ActionResult> ReloadAsync()
             => Config.Reload(Context.ServiceProvider)
                 ? Ok("Config reloaded!")
                 : BadRequest("Something bad happened. Check console for more detailed information.");
