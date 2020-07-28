bc2230c5fa49c40d55dbaa37d10e9cbb8d25733f
Baris Yildiz <barisyil97@gmail.com>
2020-07-29 01:51:06 +0200

    SetGameCommand and SetStreamCommand are now independent from each other

--- src/Commands/Modules/BotOwner/SetGameCommand.cs
+++ src/Commands/Modules/BotOwner/SetGameCommand.cs
@@ -1,7 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using Discord;
 using Qmmands;
-using Volte.Core.Attributes;
+using System.Threading.Tasks;
 using Volte.Commands.Results;
+using Volte.Core.Attributes;
 
 namespace Volte.Commands.Modules
 {
@@ -11,7 +12,12 @@ namespace Volte.Commands.Modules
         [Description("Sets the bot's game (presence).")]
         [Remarks("setgame {String}")]
         [RequireBotOwner]
-        public Task<ActionResult> SetGameAsync([Remainder] string game) 
-            => Ok($"Set the bot's game to **{game}**.", _ => Context.Client.SetGameAsync(game));
+        public Task<ActionResult> SetGameAsync([Remainder] string game)
+        {
+            var activity = Context.Client.Activity;
+            return Context.Client.Activity.Type == ActivityType.Streaming
+                ? Ok($"Set the bot's game to **{game}**.", _ => Context.Client.SetGameAsync(game, (activity as StreamingGame).Url, activity.Type))
+                : Ok($"Set the bot's game to **{game}**.", _ => Context.Client.SetGameAsync(game));
+        }
     }
 }
