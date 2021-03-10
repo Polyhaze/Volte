b4d201594ed49080a35ba62dfc8e40131940fa62
Evan Husted <greem@greemdev.net>
2021-03-10 00:17:31 -0600

    fix Say and SilentSay replies

--- src/Commands/Modules/Utility/SayCommand.cs
+++ src/Commands/Modules/Utility/SayCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Discord;
+using Gommon;
 using Qmmands;
 using Volte.Core;
 using Volte.Commands.Results;
@@ -12,14 +13,23 @@ namespace Volte.Commands.Modules
         [Description("Bot repeats what you tell it to.")]
         [Remarks("say {String}")]
         public Task<ActionResult> SayAsync([Remainder] string msg) 
-            => Ok(msg, _ => Context.Message.DeleteAsync());
+            => None(async () =>
+            {
+                await Context.CreateEmbed(msg).SendToAsync(Context.Channel);
+                await Context.Message.DeleteAsync();
+            });
 
         [Command("SilentSay", "SSay")]
         [Description("Runs the say command normally, but doesn't show the author in the message. Useful for announcements.")]
         [Remarks("silentsay {String}")]
         public Task<ActionResult> SilentSayAsync([Remainder] string msg) 
-            => Ok(new EmbedBuilder()
-                .WithColor(Config.SuccessColor)
-                .WithDescription(msg), _ => Context.Message.DeleteAsync());
+            => None(async () =>
+            {
+                await new EmbedBuilder()
+                    .WithColor(Config.SuccessColor)
+                    .WithDescription(msg)
+                    .SendToAsync(Context.Channel);
+                await Context.Message.DeleteAsync();
+            });
     }
 }
