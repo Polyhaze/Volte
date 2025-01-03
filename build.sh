d9c82568654ccb0aceed9a8e7a58d52b9004f8cf
Evan Husted <greem@greemdev.net>
2025-01-03 04:39:35 -0600

    Improve build script

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-dotnet publish -c release -r linux-arm64 --self-contained true && dotnet publish -c release -r linux-x64 --self-contained true && dotnet publish -c release -r win-x64 --self-contained true
+cd src/Bot
+
+dotnet publish -c release -r linux-arm64 --self-contained true -o ../../build/linux-arm64
+
+dotnet publish -c release -r linux-x64 --self-contained true -o ../../build/linux-x64
+
+dotnet publish -c release -r win-x64 --self-contained true -o ../../build/win-x64
