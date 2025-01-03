6a6c37f10f6182fbc8ac680c5c8663efe96e16df
Evan Husted <greem@greemdev.net>
2025-01-03 04:19:52 -0600

    oopsie duplicate definition

--- src/Bot/Commands/Modules/Utility/TextCommands.cs
+++ src/Bot/Commands/Modules/Utility/TextCommands.cs
@@ -4,36 +4,6 @@ namespace Volte.Commands.Text.Modules;
 
 public partial class UtilityModule
 {
-    [Command("Say")]
-    [Description("Bot repeats what you tell it to.")]
-    public Task<ActionResult> SayAsync([Remainder, Description("What to say.")]
-        string msg)
-        => None(async () =>
-        {
-            await Context.CreateEmbed(msg).SendToAsync(Context.Channel);
-            _ = await Context.Message.TryDeleteAsync();
-        });
-
-    [Command("SilentSay", "SSay")]
-    [Description(
-        "Runs the say command normally, but doesn't show the author in the message.")]
-    public Task<ActionResult> SilentSayAsync([Remainder, Description("What to say.")]
-        string msg)
-        => None(async () =>
-        {
-            await new EmbedBuilder()
-                .WithColor(Config.SuccessColor)
-                .WithDescription(msg)
-                .SendToAsync(Context.Channel);
-            _ = await Context.Message.TryDeleteAsync();
-        });
-
-    [Command("PlainSay", "PSay")]
-    [Description("Bot repeats what you tell it to; outside of an embed.")]
-    public Task<ActionResult> SayPlainAsync([Remainder, Description("What to say.")]
-        string msg)
-        => None(() => Context.Channel.SendMessageAsync(msg, allowedMentions: AllowedMentions.None));
-
     [Command("Reverse" /*, "esreveR"*/)]
     [Description("Bot replies with the argument value reversed.")]
     public Task<ActionResult> ReverseAsync([Remainder, Description("What to reverse.")]
