0c5637abbfd20893ea293bd6f7dc906eb14539d5
GreemDev <greem@greemdev.net>
2025-04-14 22:14:33 -0500

    silent plain say command

--- src/Bot/Commands/Modules/Moderation/TextCommands.cs
+++ src/Bot/Commands/Modules/Moderation/TextCommands.cs
@@ -28,7 +28,18 @@ public partial class ModerationModule
 
     [Command("PlainSay", "PSay")]
     [Description("Bot repeats what you tell it to; outside of an embed.")]
-    public Task<ActionResult> SayPlainAsync([Remainder, Description("What to say.")]
+    public Task<ActionResult> PlainSayAsync([Remainder, Description("What to say.")]
         string msg)
         => None(() => Context.Channel.SendMessageAsync(msg, allowedMentions: AllowedMentions.None));
+    
+    [Command("SilentPlainSay", "SPSay")]
+    [Description(
+        "Runs the say command normally, but doesn't show the author in the message.")]
+    public Task<ActionResult> SilentPlainSayAsync([Remainder, Description("What to say.")]
+        string msg)
+        => None(async () =>
+        {
+            await Context.Channel.SendMessageAsync(msg, allowedMentions: AllowedMentions.None);
+            await Context.Message.TryDeleteAsync();
+        });
 }
