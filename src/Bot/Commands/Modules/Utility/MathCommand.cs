9e24876cd9d691d95c87bfb5573a34845ffbb7c9
GreemDev <greemdev@ryujinx.app>
2025-07-20 00:51:20 -0500

    Add Starscript.Net and a math command

---
+++ src/Bot/Commands/Modules/Utility/MathCommand.cs
@@ -0,0 +1,31 @@
+﻿using Starscript;
+using Starscript.Internal;
+
+namespace Volte.Commands.Text.Modules;
+
+public sealed partial class UtilityModule
+{
+    [Command("Math", "Maths")]
+    [Description("Evaluate a mathematical expression.")]
+    public Task<ActionResult> MathAsync(
+        [Remainder, Description("The expression.")]
+        string expression)
+    {
+        try
+        {
+            var result = VolteStarscript.RunMath(expression);
+
+            return Ok(Context.CreateEmbedBuilder()
+                .AddField("Input", Format.Code(expression))
+                .AddField("Output", Format.Code(result.ToString())));
+        }
+        catch (ParseException pe)
+        {
+            return BadRequest($"Syntax error: {pe.Message}");
+        }
+        catch (StarscriptException se)
+        {
+            return BadRequest(se.Message);
+        }
+    }
+}
---
+++ src/Bot/Helpers/VolteStarscript.cs
@@ -0,0 +1,24 @@
+﻿using Starscript;
+using Starscript.Internal;
+
+namespace Volte.Helpers;
+
+public static class VolteStarscript
+{
+    public static readonly StarscriptHypervisor Hypervisor = StarscriptHypervisor.CreateWithStdLib();
+
+    public static readonly StarscriptHypervisor MathHypervisor = StarscriptHypervisor.CreateStandalone().WithStandardLibraryMath();
+
+    public static StringSegment RunMath(string expression)
+        => CompileMath(expression).Execute(MathHypervisor);
+    
+    public static Script Compile(string source)
+    {
+        if (!Parser.TryParse(source, out var result))
+            throw new ParseException(result!.Errors.First());
+
+        return Compiler.SingleCompile(result);
+    }
+
+    public static Script CompileMath(string source) => Compile($"{{{source}}}");
+}
