1c75e978e92cf6c89c7ad4e9a1819c83f6138b71
GreemDev <greemdev@ryujinx.app>
2025-07-26 02:15:42 -0500

    add slash command starscript autocompleters

---
+++ src/Bot/Interactions/Commands/Autocompleters/Starscript/VolteStarscriptAutocompleter.cs
@@ -0,0 +1,8 @@
+﻿using Starscript;
+
+namespace Volte.Interactions.Commands;
+
+public class VolteStarscriptAutocompleter : AbstractStarscriptAutocompleter
+{
+    public override StarscriptHypervisor Hypervisor => VolteStarscript.Hypervisor;
+}
