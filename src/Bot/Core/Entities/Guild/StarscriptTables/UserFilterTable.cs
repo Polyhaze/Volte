12e031519af88f107ede73b81f6378ee61e4e7bd
GreemDev <greemdev@ryujinx.app>
2025-07-26 15:54:11 -0500

    delegate handling logic for userfilter to the userfilter table add debug log lines for exceptions in the starscript pipeline

--- src/Bot/Core/Entities/Guild/StarscriptTables/UserFilterTable.cs
+++ src/Bot/Core/Entities/Guild/StarscriptTables/UserFilterTable.cs
@@ -1,4 +1,7 @@
-﻿namespace Volte.Entities;
+﻿using Starscript;
+using Starscript.Internal;
+
+namespace Volte.Entities;
 
 public class UserFilterTable
 {
@@ -17,6 +20,15 @@ public class UserFilterTable
                 Type = StarscriptType.SingleExpression
             }
         });
+    
+    public async IAsyncEnumerable<uint> HandleAsync(IGuild guild, IGuildUser user)
+    {
+        foreach (var entry in Entries)
+        {
+            if (!await entry.HandleAsync(guild, user))
+                yield return entry.Id;
+        }
+    }
 }
 
 public class UserFilterEntry
@@ -28,6 +40,58 @@ public class UserFilterEntry
     public ActionType Action { get; set; }
     
     public string Reason { get; set; }
+
+    /// <summary>
+    ///     Checks if this entry matches the proviced <paramref name="user"/> in <paramref name="guild"/>, and if they do, calls <see cref="ExecuteAsync"/> on them.
+    /// </summary>
+    /// <param name="guild">The guild to perform moderation actions from.</param>
+    /// <param name="user">The user to filter.</param>
+    /// <returns>true if the underlying <see cref="StarscriptSrc"/> compilation and execution succeeds, AND returns 'true' or 'false'; false otherwise.</returns>
+    public async Task<bool> HandleAsync(IGuild guild, IGuildUser user)
+    {
+        StringSegment returnVal = null;
+        
+        try
+        {
+            var script = Starscript.Compile();
+
+            returnVal = VolteStarscript.Hypervisor.Run(script, StarscriptHelper.Wrap(user));
+
+            if (returnVal.GetBooleanValue())
+            {
+                if (await ExecuteAsync(guild, user))
+                {
+                    Debug(LogSource.Service, $"{user} matched filter {Id} in guild {user.Guild.Id}");
+                }
+            }
+
+            return true;
+        }
+        catch (ParseException e)
+        {
+            // Remove entries that have parsing errors
+            Debug(LogSource.Service, $"Filter {Id} in guild {user.Guild.Id} failed parsing:");
+            Debug(LogSource.Service, e.Error.ToString());
+            return false;
+        }
+        catch (StarscriptException e)
+        {
+            // Remove entries that have execution errors
+            Debug(LogSource.Service, $"Filter {Id} in guild {user.Guild.Id} failed execution:");
+            Debug(LogSource.Service, e.Message);
+            return false;
+        }
+        catch (FormatException e)
+        {
+            // Remove entries that do not return true/false
+            Debug(LogSource.Service, 
+                $"Filter {Id} in guild {user.Guild.Id}'s result was not true/false:" +
+                $"{(returnVal != null ? $" '{returnVal}'" : string.Empty)}");
+            if (!e.Message.IsNullOrEmpty()) 
+                Debug(LogSource.Service, e.Message);
+            return false;
+        }
+    }
     
     /// <summary>
     ///     Performs the action specified by <see cref="Action"/> with the given <see cref="Reason"/>.
@@ -35,7 +99,7 @@ public class UserFilterEntry
     /// <param name="guild">The guild the user is in.</param>
     /// <param name="user">The user to moderate.</param>
     /// <returns>true if an action was carried out successfully; false if there was an <see cref="HttpException"/>.</returns>
-    public async Task<bool> ExecuteAsync(SocketGuild guild, IGuildUser user)
+    public async Task<bool> ExecuteAsync(IGuild guild, IGuildUser user)
     {
         switch (Action)
         {
--- src/Bot/Services/UserFilterService.cs
+++ src/Bot/Services/UserFilterService.cs
@@ -1,6 +1,4 @@
-﻿using Starscript.Internal;
-
-namespace Volte.Services;
+﻿namespace Volte.Services;
 
 public class UserFilterService : VolteService
 {
@@ -14,49 +12,14 @@ public class UserFilterService : VolteService
     public async Task OnUserJoined(UserJoinedEventArgs args)
     {
         var data = await _db.GetDataAsync(args.Guild.Id);
-        var idsToClear = await HandleMultipleScripts(args.Guild, args.User, data.Extras.StarscriptTables.UserFilter).ToArrayAsync();
+        var idsToClear = await data.Extras.StarscriptTables.UserFilter
+            .HandleAsync(args.Guild, args.User)
+            .ToArrayAsync();
 
         if (idsToClear.Length > 0)
         {
             data.Extras.StarscriptTables.UserFilter.Entries.RemoveAll(it => idsToClear.Contains(it.Id));
             _db.Save(data);
-        }
-    }
-    
-    private async IAsyncEnumerable<uint> HandleMultipleScripts(SocketGuild guild, SocketGuildUser user, UserFilterTable table)
-    {
-        foreach (var entry in table.Entries)
-        {
-            if (await HandleSingleScript(guild, user, entry) is { } id)
-                yield return id;
-        }
-    }
-    
-    private async Task<uint?> HandleSingleScript(SocketGuild guild, SocketGuildUser user, UserFilterEntry entry)
-    {
-        try
-        {
-            var script = entry.Starscript.Compile();
-
-            if (VolteStarscript.Hypervisor.Run(script, StarscriptHelper.Wrap(user)).GetBooleanValue())
-            {
-                if (await entry.ExecuteAsync(guild, user))
-                {
-                    Debug(LogSource.Service, $"{user} matched filter {entry.Id} in guild {user.Guild.Id}");
-                }
-            }
-
-            return null;
-        }
-        catch (ParseException)
-        {
-            // Remove entries that have parsing errors
-            return entry.Id;
-        }
-        catch (FormatException)
-        {
-            // Remove entries that do not return true/false
-            return entry.Id;
         }
     }
 }
