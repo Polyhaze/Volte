cd06c9b49be1e11db31a3bbfdc9b352664c437fd
GreemDev <greemdev@ryujinx.app>
2025-07-28 22:56:51 -0500

    selfrole autocompleters

---
+++ src/Bot/Interactions/Autocompleters/SelfRoleAssignAutocompleter.cs
@@ -0,0 +1,37 @@
+﻿using Discord.Interactions;
+
+namespace Volte.Interactions;
+
+public class SelfRoleAssignAutocompleter : AutocompleteHandler
+{
+    public override async Task<AutocompletionResult> GenerateSuggestionsAsync(
+        IInteractionContext context, 
+        IAutocompleteInteraction autocompleteInteraction,
+        IParameterInfo parameter, 
+        IServiceProvider services)
+    {
+        if (context.Guild is not SocketGuild guild) 
+            return AutocompletionResult.FromSuccess();
+        
+        var guildData = services.Get<DatabaseService>().GetData(context.Guild);
+        
+        foreach (var option in autocompleteInteraction.Data.Options)
+        {
+            var userValue = option.Value?.ToString();
+
+            if (!option.Focused || string.Empty.Equals(userValue) || userValue == null) continue;
+            
+            var selfRoles = guild.Roles.Where(x => guildData.Extras.SelfRoles.Contains(x.Id));
+
+            var validSelfRoles = selfRoles.Except(guild.GetUser(context.User.Id).Roles).ToList();
+
+            if (validSelfRoles.Count > 0)
+            {
+                return AutocompletionResult.FromSuccess(validSelfRoles
+                    .Select(x => new AutocompleteResult(x.Name, x.Id)));
+            }
+        }
+
+        return AutocompletionResult.FromSuccess();
+    }
+}
---
+++ src/Bot/Interactions/Autocompleters/SelfRoleResignAutocompleter.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Interactions;
+
+namespace Volte.Interactions;
+
+public class SelfRoleResignAutocompleter : AutocompleteHandler
+{
+    public override async Task<AutocompletionResult> GenerateSuggestionsAsync(
+        IInteractionContext context, 
+        IAutocompleteInteraction autocompleteInteraction,
+        IParameterInfo parameter, 
+        IServiceProvider services)
+    {
+        if (context.Guild is not SocketGuild guild) 
+            return AutocompletionResult.FromSuccess();
+        
+        var guildData = services.Get<DatabaseService>().GetData(context.Guild);
+
+        if (guild.GetUser(context.User.Id) is not {} user)
+            return AutocompletionResult.FromSuccess();
+        
+        foreach (var option in autocompleteInteraction.Data.Options)
+        {
+            var userValue = option.Value?.ToString();
+
+            if (!option.Focused || string.Empty.Equals(userValue) || userValue == null) continue;
+            
+            var selfRoles = guild.Roles.Where(x => guildData.Extras.SelfRoles.Contains(x.Id));
+
+            var validSelfRoles = selfRoles.Where(it => user.HasRole(it.Id)).ToList();
+
+            if (validSelfRoles.Count > 0)
+            {
+                return AutocompletionResult.FromSuccess(validSelfRoles
+                    .Select(x => new AutocompleteResult(x.Name, x.Id)));
+            }
+        }
+
+        return AutocompletionResult.FromSuccess();
+    }
+}
