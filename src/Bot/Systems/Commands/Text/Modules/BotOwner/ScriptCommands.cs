026e7e8acbfd708c6ebcfc101e2014b0449aa480
GreemDev <greemdev@ryujinx.app>
2025-07-28 23:58:59 -0500

    update eval environment URL for new source location

--- src/Bot/Systems/Commands/Text/Modules/BotOwner/ScriptCommands.cs
+++ src/Bot/Systems/Commands/Text/Modules/BotOwner/ScriptCommands.cs
@@ -2,7 +2,7 @@ namespace Volte.Systems.Commands.Text.Modules;
 
 public sealed partial class BotOwnerModule
 {
-    private const string EvalEnvSource = "https://github.com/Polyhaze/Volte/blob/v4/src/Bot/Core/Entities/EvalEnvironment.cs";
+    private const string EvalEnvSource = "https://github.com/Polyhaze/Volte/blob/v4/src/Bot/Entities/EvalEnvironment.cs";
     
     [Command("Evaluate", "Eval", "Repl")]
     [Description($"Evaluates C# code. You have implicit access to the methods defined in [EvalEnvironment]({EvalEnvSource}).")]
