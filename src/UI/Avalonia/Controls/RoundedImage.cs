ff24f1c98d587779b5d9c11169826e96aaf407b3
GreemDev <greemdev@ryujinx.app>
2025-08-01 02:31:46 -0500

    [ci skip] make roundedimage cornerradius a normal instance property instead of static get-set style

--- src/UI/Avalonia/Controls/RoundedImage.cs
+++ src/UI/Avalonia/Controls/RoundedImage.cs
@@ -9,12 +9,14 @@ public class RoundedImage : CustomImageClipControl
     public static readonly AttachedProperty<double> CornerRadiusProperty =
         AvaloniaProperty.RegisterAttached<RoundedImage, double>(
             "CornerRadius", typeof(RoundedImage), 5);
-    
-    public static void SetCornerRadius(AvaloniaObject element, double parameter) => element.SetValue(CornerRadiusProperty, parameter);
 
-    public static double GetCornerRadius(AvaloniaObject element) => element.GetValue(CornerRadiusProperty);
+    public double CornerRadius
+    {
+        get => GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
 
 
     public override Geometry GetClip(Size size) 
-        => new RectangleGeometry(new Rect(size), GetCornerRadius(this), GetCornerRadius(this));
+        => new RectangleGeometry(new Rect(size), CornerRadius, CornerRadius);
 }
