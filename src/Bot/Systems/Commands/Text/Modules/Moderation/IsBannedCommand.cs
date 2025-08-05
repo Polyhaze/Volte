009c91e360c6fcc614f2c6cfc1293387145ada3f
GreemDev <greemdev@ryujinx.app>
2025-08-05 01:42:44 -0500

    IsBanned command

---
+++ src/Bot/Systems/Commands/Text/Modules/Moderation/IsBannedCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Volte.Systems.Commands.Text.Modules;
+
+public partial class ModerationModule
+{
+    [Command("IsBanned")]
+    [Description("Check if a user is banned. Useful for large servers.")]
+    public async Task<ActionResult> IsBannedAsync([Description("The user to check.")] RestUser user)
+    {
+        if (await Context.Guild.GetBanAsync(user) is { } ban)
+            return Ok($"Yes; {Format.Bold(Format.Sanitize(user.Username))} is banned for: {Format.Code(ban.Reason, string.Empty)}");
+        
+        return Ok($"Nope. {Format.Bold(Format.Sanitize(user.Username))} is not banned.");
+    }
+}
