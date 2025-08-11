a83d0477da4dd87d63916bf0731c287952e2b78f
GreemDev <greemdev@ryujinx.app>
2025-08-11 15:51:35 -0500

    allow check attributes to work on more than just SocketGuildUser

--- src/Bot/Systems/Commands/Text/Attributes/Checks/CheckHierarchyAttribute.cs
+++ src/Bot/Systems/Commands/Text/Attributes/Checks/CheckHierarchyAttribute.cs
@@ -5,14 +5,20 @@ public sealed class CheckHierarchyAttribute : ParameterCheckAttribute
 {
     public override ValueTask<CheckResult> CheckAsync(object argument, CommandContext context)
     {
-        var u = argument.Cast<SocketGuildUser>() ?? throw new ArgumentException(
-            $"Cannot use the CheckHierarchy attribute on a type that isn't {typeof(SocketGuildUser)}.");
         var ctx = context.Cast<VolteContext>();
+
+        if (argument is RestUser ru && ctx.Guild.GetUser(ru.Id) is { } usr)
+            argument = usr;
+
+        if (argument is not SocketGuildUser u)
+        {
+            return Success(); //non-members should be considered hierarchically lower
+        }
 
         return ctx.IsAdmin(u)
             ? CheckResult.Failed("Cannot ban someone with the configured Admin role.")
             : ctx.User.Hierarchy > u.Hierarchy
                 ? CheckResult.Successful
-                : CheckResult.Failed("Cannot ban someone in a higher, or equal, hierarchy position than yourself.");
+                : CheckResult.Failed("Cannot act on someone in a higher, or equal, hierarchy position than yourself.");
     }
 }
