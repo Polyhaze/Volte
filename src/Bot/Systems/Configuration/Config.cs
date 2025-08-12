d4c8d9d5df5664cb14b4c9d0867b23bfcdbf996a
GreemDev <greemdev@ryujinx.app>
2025-08-12 00:06:06 -0500

    move the config stuff & add the ability to ignore specific debug log messagess to the config

--- src/Bot/Systems/Configuration/Config.cs
+++ src/Bot/Systems/Configuration/Config.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Volte;
+namespace Volte.Systems.Configuration;
 
 public static class Config
 {
@@ -12,17 +12,14 @@ public static class Config
     public static readonly FilePath Path = FilePath.Data / "volte.json";
 
     private static JsonSerializerOptions CreateSerializerOptions(bool writeIndented)
-        => new JsonSerializerOptions
+        => new()
         {
             ReadCommentHandling = JsonCommentHandling.Skip,
             WriteIndented = writeIndented,
             PropertyNameCaseInsensitive = true,
             DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
             AllowTrailingCommas = true
-        }.Apply(opt =>
-        {
-            opt.Converters.Add(new Snowflake.JsonConverter());
-        });
+        };
     
 
     private static bool IsValidConfig() 
@@ -138,13 +135,15 @@ public static class Config
 
     public static string SentryDsn => _configuration.SentryDsn;
 
-    public static Snowflake Owner => _configuration.Owner;
+    public static ulong Owner => _configuration.Owner;
 
     public static string Game => _configuration.Game;
 
     public static string Streamer => _configuration.Streamer;
 
     public static bool DebugEnabled => _configuration?.EnableDebug ?? false;
+    
+    public static string[] IgnoredDebugLines => _configuration?.IgnoredDebugMessages ?? [];
 
     public static string FormattedStreamUrl => $"https://twitch.tv/{Streamer}";
 
@@ -154,85 +153,7 @@ public static class Config
 
     public static bool LogAllCommands => _configuration.LogAllCommands;
 
-    public static HashSet<Snowflake> BlacklistedOwners => _configuration.BlacklistedGuildOwners;
+    public static HashSet<ulong> BlacklistedOwners => _configuration.BlacklistedGuildOwners;
 
     public static EnabledFeatures EnabledFeatures => _configuration?.EnabledFeatures;
-}
-
-public struct HeadlessBotConfig : IVolteConfig
-{
-    [JsonPropertyName("discord_token")]
-    public string Token { get; set; }
-            
-    [JsonPropertyName("sentry_dsn")]
-    public string SentryDsn { get; set; }
-
-    [JsonPropertyName("command_prefix")]
-    public string CommandPrefix { get; set; }
-
-    [JsonPropertyName("bot_owner")]
-    public Snowflake Owner { get; set; }
-
-    [JsonPropertyName("status_game")]
-    public string Game { get; set; }
-
-    [JsonPropertyName("status_twitch_streamer")]
-    public string Streamer { get; set; }
-
-    [JsonPropertyName("enable_debug_logging")]
-    public bool EnableDebug { get; set; }
-
-    [JsonPropertyName("color_success")]
-    public uint SuccessEmbedColor { get; set; }
-
-    [JsonPropertyName("color_error")]
-    public uint ErrorEmbedColor { get; set; }
-
-    [JsonPropertyName("log_all_commands")]
-    public bool LogAllCommands { get; set; }
-
-    [JsonPropertyName("blacklisted_guild_owners")]
-    public HashSet<Snowflake> BlacklistedGuildOwners { get; set; }
-
-    [JsonPropertyName("enabled_features")]
-    public EnabledFeatures EnabledFeatures { get; set; }
-}
-
-public interface IVolteConfig
-{
-    [JsonPropertyName("discord_token")]
-    public string Token { get; set; }
-            
-    [JsonPropertyName("sentry_dsn")]
-    public string SentryDsn { get; set; }
-
-    [JsonPropertyName("command_prefix")]
-    public string CommandPrefix { get; set; }
-
-    [JsonPropertyName("bot_owner")]
-    public Snowflake Owner { get; set; }
-
-    [JsonPropertyName("status_game")]
-    public string Game { get; set; }
-
-    [JsonPropertyName("status_twitch_streamer")]
-    public string Streamer { get; set; }
-
-    [JsonPropertyName("enable_debug_logging")]
-    public bool EnableDebug { get; set; }
-
-    [JsonPropertyName("color_success")]
-    public uint SuccessEmbedColor { get; set; }
-
-    [JsonPropertyName("color_error")]
-    public uint ErrorEmbedColor { get; set; }
-
-    [JsonPropertyName("log_all_commands")]
-    public bool LogAllCommands { get; set; }
-
-    [JsonPropertyName("blacklisted_guild_owners")]
-    public HashSet<Snowflake> BlacklistedGuildOwners { get; set; }
-
-    [JsonPropertyName("enabled_features")]
-    public EnabledFeatures EnabledFeatures { get; set; }
 }
---
+++ src/Bot/Systems/Configuration/Types/HeadlessBotConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Volte.Systems.Configuration;
+
+public struct HeadlessBotConfig : IVolteConfig
+{
+    [JsonPropertyName("discord_token")]
+    public string Token { get; set; }
+            
+    [JsonPropertyName("sentry_dsn")]
+    public string SentryDsn { get; set; }
+
+    [JsonPropertyName("command_prefix")]
+    public string CommandPrefix { get; set; }
+
+    [JsonPropertyName("bot_owner")]
+    public ulong Owner { get; set; }
+
+    [JsonPropertyName("status_game")]
+    public string Game { get; set; }
+
+    [JsonPropertyName("status_twitch_streamer")]
+    public string Streamer { get; set; }
+
+    [JsonPropertyName("enable_debug_logging")]
+    public bool EnableDebug { get; set; }
+    
+    [JsonPropertyName("ignored_debug_log_messages")]
+    public string[] IgnoredDebugMessages { get; set; }
+
+    [JsonPropertyName("color_success")]
+    public uint SuccessEmbedColor { get; set; }
+
+    [JsonPropertyName("color_error")]
+    public uint ErrorEmbedColor { get; set; }
+
+    [JsonPropertyName("log_all_commands")]
+    public bool LogAllCommands { get; set; }
+
+    [JsonPropertyName("blacklisted_guild_owners")]
+    public HashSet<ulong> BlacklistedGuildOwners { get; set; }
+
+    [JsonPropertyName("enabled_features")]
+    public EnabledFeatures EnabledFeatures { get; set; }
+}
