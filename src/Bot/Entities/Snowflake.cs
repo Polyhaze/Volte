e8fe1ca0bc0104d897a7624727a696592d05315b
GreemDev <greemdev@ryujinx.app>
2025-08-13 16:18:58 -0500

    add snowflake json converter to v4

--- src/Bot/Entities/Snowflake.cs
+++ src/Bot/Entities/Snowflake.cs
@@ -1,6 +1,8 @@
 ﻿using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace Volte.Entities;
+
 
 public readonly record struct Snowflake(ulong Raw) : IComparable<Snowflake>, IComparable, IParsable<Snowflake>
 {
@@ -20,15 +22,16 @@ public readonly record struct Snowflake(ulong Raw) : IComparable<Snowflake>, ICo
 
     public DateTimeOffset Date => SnowflakeUtils.FromSnowflake(Raw);
 
+    public override string ToString() => Raw.ToString();
     public static implicit operator Snowflake(ulong raw) => new(raw);
     public static implicit operator ulong(Snowflake snowflake) => snowflake.Raw;
     public static implicit operator Snowflake(DateTimeOffset dto) => FromDate(dto);
     public static implicit operator DateTimeOffset(Snowflake snowflake) => snowflake.Date;
     public static implicit operator Snowflake(DateTime dto) => FromDate(dto);
     public static implicit operator DateTime(Snowflake snowflake) => snowflake.Date.DateTime;
-    
+
     public static Snowflake Zero { get; set; } = default;
-    
+
     public static Snowflake FromDate(DateTimeOffset dto) => new(SnowflakeUtils.ToSnowflake(dto));
 
     public static Snowflake? TryParse(string value, IFormatProvider formatProvider = null)
@@ -46,32 +49,44 @@ public readonly record struct Snowflake(ulong Raw) : IComparable<Snowflake>, ICo
 
         return null;
     }
-    
-    public static Snowflake Parse(string s, IFormatProvider provider) =>
-        TryParse(s, provider, out var result)
-            ? result
-            : throw new ArgumentException($"'{s}' did not contain a valid 64-bit unsigned integer or DateTimeOffset.");
+
+    public static Snowflake Parse(string s, IFormatProvider provider)
+        => TryParse(s, provider)
+           ?? throw new ArgumentException($"'{s}' did not contain a valid 64-bit unsigned integer or DateTimeOffset.");
 
     public static bool TryParse(string s, IFormatProvider provider, out Snowflake result)
     {
-        result = Zero;
-        if (string.IsNullOrWhiteSpace(s))
-            return false;
+        var res = TryParse(s, provider);
+        result = res ?? Zero;
 
-        // As number
-        if (ulong.TryParse(s, NumberStyles.None, provider, out var number))
+        return res.HasValue;
+    }
+
+    public static bool TryParse(string s, out Snowflake result)
+    {
+        var res = TryParse(s);
+        result = res ?? Zero;
+
+        return res.HasValue;
+    }
+
+    public class JsonConverter : JsonConverter<Snowflake>
+    {
+        public override Snowflake Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            result = new Snowflake(number);
-            return true;
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.String:
+                    return Parse(reader.GetString()!, null);
+                case JsonTokenType.Number:
+                    return new Snowflake(reader.GetUInt64());
+                default:
+                    throw new InvalidOperationException(
+                        "Snowflake JSON value is expected to be a datetime/integer string or a raw integer. Prefer raw integers.");
+            }
         }
 
-        // As date
-        if (DateTimeOffset.TryParse(s, provider, DateTimeStyles.None, out var dto))
-        {
-            result = FromDate(dto);
-            return true;
-        }
-
-        return false;
+        public override void Write(Utf8JsonWriter writer, Snowflake value, JsonSerializerOptions options)
+            => writer.WriteNumberValue(value.Raw);
     }
 }
