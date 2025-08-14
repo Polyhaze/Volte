47cd4853e600451c55e1b98944dae3a676d0cb7b
GreemDev <greemdev@ryujinx.app>
2025-08-14 00:46:12 -0500

    add more handlers

--- src/Bot/Systems/Moderation/AuditLog/Handlers/UserModeration.cs
+++ src/Bot/Systems/Moderation/AuditLog/Handlers/UserModeration.cs
@@ -5,12 +5,42 @@ public static partial class AuditLogHandlers
     [AuditLogHandler<SocketBanAuditLogData>(ActionType.Ban)]
     private static async Task Ban(AuditLogContext<SocketBanAuditLogData> ctx)
     {
+        var reason = string.IsNullOrEmpty(ctx.Entry.Reason) ? "None provided" : ctx.Entry.Reason;
+
         var target = await ctx.Data.Target.GetOrDownloadAsync();
 
         ctx.Message.WithColor(Color.DarkRed)
             .WithTitle("User banned")
             .WithThumbnailUrl(target.GetDisplayAvatarUrl())
             .AddField("Target", $"{target.Username} ({target.Id})")
-            .AddField("Reason", Format.Code(ctx.Entry.Reason, string.Empty));
+            .AddField("Reason", Format.Code(reason, string.Empty));
+    }
+
+    [AuditLogHandler<SocketKickAuditLogData>(ActionType.Kick)]
+    private static async Task Kick(AuditLogContext<SocketKickAuditLogData> ctx)
+    {
+        var reason = string.IsNullOrEmpty(ctx.Entry.Reason) ? "None provided" : ctx.Entry.Reason;
+
+        var target = await ctx.Data.Target.GetOrDownloadAsync();
+
+        ctx.Message.WithColor(Color.DarkRed)
+            .WithTitle("User kicked")
+            .WithThumbnailUrl(target.GetDisplayAvatarUrl())
+            .AddField("Target", $"{target.Username} ({target.Id})")
+            .AddField("Reason", Format.Code(reason, string.Empty));
+    }
+
+    [AuditLogHandler<SocketUnbanAuditLogData>(ActionType.Unban)]
+    private static async Task Unban(AuditLogContext<SocketUnbanAuditLogData> ctx)
+    {
+        var reason = string.IsNullOrEmpty(ctx.Entry.Reason) ? "None provided" : ctx.Entry.Reason;
+
+        var target = await ctx.Data.Target.GetOrDownloadAsync();
+
+        ctx.Message.WithColor(Color.DarkRed)
+            .WithTitle("User kicked")
+            .WithThumbnailUrl(target.GetDisplayAvatarUrl())
+            .AddField("Target", $"{target.Username} ({target.Id})")
+            .AddField("Reason", Format.Code(reason, string.Empty));
     }
 }
