9193d05ab36f4da24482d0b33b25136b47170ce3
GreemDev <greemdev@ryujinx.app>
2025-08-13 23:09:49 -0500

    remove WelcomeService injection from SettingsModule

--- src/Bot/Systems/Commands/Text/Modules/ModuleCommonBases.cs
+++ src/Bot/Systems/Commands/Text/Modules/ModuleCommonBases.cs
@@ -145,8 +145,6 @@ public sealed partial class ModerationModule : VolteModule
 [RequireGuildAdmin]
 public partial class SettingsModule : VolteModule
 {
-    public WelcomeService WelcomeService { get; set; }
-
     [Command, DummyCommand, Description("The set of commands used to modify how Volte functions in your guild.")]
     public Task<ActionResult> BaseAsync() => None();
 }
