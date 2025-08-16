73b9520b9435ada967161ddc3426b9584566d089
GreemDev <greemdev@ryujinx.app>
2025-08-16 00:36:35 -0500

    wrong precondition type

--- src/Bot/Systems/Commands/Interaction/Modules/InteractionSettingsModule.cs
+++ src/Bot/Systems/Commands/Interaction/Modules/InteractionSettingsModule.cs
@@ -1,7 +1,7 @@
 ﻿namespace Volte.Systems.Commands.Interaction.Modules;
 
 [Discord.Interactions.Group("settings", "View & modify settings in your guild. Admin only.")]
-[RequireGuildAdmin]
+[RequireGuildAdminPrecondition]
 public partial class InteractionSettingsModule : VolteSlashCommandModule
 {
     [Discord.Interactions.Group("welcome", "View & modify settings related to the welcome system. Admin only.")]
