16ffaf7dd9712f8f839c652433685d2294ebbf7b
GreemDev <greemdev@ryujinx.app>
2025-08-17 21:53:39 -0500

    run userfilter in a background task

--- src/Bot/Systems/UserFilter/UserFilterService.cs
+++ src/Bot/Systems/UserFilter/UserFilterService.cs
@@ -11,17 +11,22 @@ public class UserFilterService : VolteService
         _db = databaseService;
     }
 
-    public async Task OnUserJoined(UserJoinedEventArgs args)
+    public Task OnUserJoined(UserJoinedEventArgs args)
     {
-        var data = await _db.GetDataAsync(args.Guild.Id);
-        var idsToClear = await data.StarscriptTables.UserFilter
-            .HandleAsync(args.Guild, args.User)
-            .ToArrayAsync();
-
-        if (idsToClear.Length > 0)
+        ExecuteBackgroundAsync(async () =>
         {
-            data.StarscriptTables.UserFilter.Entries.RemoveAll(it => idsToClear.Contains(it.Id));
-            _db.Save(data);
-        }
+            var data = await _db.GetDataAsync(args.Guild.Id);
+            var idsToClear = await data.StarscriptTables.UserFilter
+                .HandleAsync(args.Guild, args.User)
+                .ToArrayAsync();
+
+            if (idsToClear.Length > 0)
+            {
+                data.StarscriptTables.UserFilter.Entries.RemoveAll(it => idsToClear.Contains(it.Id));
+                _db.Save(data);
+            }
+        });
+
+        return Task.CompletedTask;
     }
 }
