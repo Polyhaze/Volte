f0f54cf22d42d6c607e5c86376e9a1fad516241d
GreemDev <greemdev@ryujinx.app>
2025-08-18 20:49:57 -0500

    remove leftovers from imgui

--- src/Bot/Helpers/Extensions/MiscExtensions.cs
+++ src/Bot/Helpers/Extensions/MiscExtensions.cs
@@ -9,26 +9,8 @@ namespace Gommon;
 
 public partial class Extensions
 {
-    public static System.Drawing.Color AsColor(this Vector3 vec3)
-        => System.Drawing.Color.FromArgb(255,
-            (int)(vec3.X.CoerceAtMost(1) * 255),
-            (int)(vec3.Y.CoerceAtMost(1) * 255),
-            (int)(vec3.Z.CoerceAtMost(1) * 255));
-
-    public static Vector3 AsVec3(this System.Drawing.Color color)
-        => new(color.R / 255f, color.G / 255f, color.B / 255f);
-    
-    public static Vector4 AsVec4(this System.Drawing.Color color)
-        => new(color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f);
-    
-    public static Vector3 AsVec3(this Color color)
-        => new(color.R / 255f, color.G / 255f, color.B / 255f);
-    
-    public static Vector4 AsVec4(this Color color)
-        => new(color.R / 255f, color.G / 255f, color.B / 255f, 1f);
-    
     public static bool LengthEquals(this Stream stream, long exactLength) => stream.Length == exactLength;
-    
+
     public static MemoryStream CreateColorImage(this Rgba32 color, int width = 125, int height = 125) => new MemoryStream().Apply(ms =>
     {
         using var image = new Image<Rgba32>(width, height);
