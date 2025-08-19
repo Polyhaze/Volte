a1287ac7506e5fae865f185f07a7310da64f084a
GreemDev <greemdev@ryujinx.app>
2025-08-19 00:23:01 -0500

    role delete audit log event

---
+++ src/Bot/Systems/Moderation/AuditLog/Handlers/Roles.cs
@@ -0,0 +1,13 @@
+﻿namespace Volte.Systems.Moderation;
+
+public static partial class AuditLogHandlers
+{
+    [AuditLogHandler<SocketRoleDeleteAuditLogData>(ActionType.RoleDeleted)]
+    private static void RoleDeleted(AuditLogContext<SocketRoleDeleteAuditLogData> ctx)
+    {
+        ctx.Message.WithColor(Color.DarkRed)
+            .WithTitle("Role deleted")
+            .AddField("Target", $"{ctx.Data.Properties.Name} ({ctx.Data.RoleId})");
+    }
+
+}
