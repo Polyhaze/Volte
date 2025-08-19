59b55f36e1629961e1a373a41eb3dd5dbff8a1fb
GreemDev <greemdev@ryujinx.app>
2025-08-19 00:16:33 -0500

    huh, surprised that compiled

--- src/Bot/Systems/Moderation/AuditLog/Handlers/GuildUpdated.cs
+++ src/Bot/Systems/Moderation/AuditLog/Handlers/GuildUpdated.cs
@@ -1,6 +1,6 @@
 ﻿namespace Volte.Systems.Moderation;
 
-public partial class AuditLogHandlers
+public static partial class AuditLogHandlers
 {
     [AuditLogHandler<SocketGuildUpdateAuditLogData>(ActionType.GuildUpdated)]
     private static void GuildUpdated(AuditLogContext<SocketGuildUpdateAuditLogData> ctx)
