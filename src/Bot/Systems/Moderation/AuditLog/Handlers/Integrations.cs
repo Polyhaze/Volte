12a39de188ef68a18a3adbefd9797ebef516bb2d
GreemDev <greemdev@ryujinx.app>
2025-08-19 00:22:55 -0500

    fix bot add

--- src/Bot/Systems/Moderation/AuditLog/Handlers/Integrations.cs
+++ src/Bot/Systems/Moderation/AuditLog/Handlers/Integrations.cs
@@ -2,11 +2,12 @@
 
 public static partial class AuditLogHandlers
 {
-    public static async Task BotAdded(AuditLogContext<SocketBotAddAuditLogData> ctx)
+    [AuditLogHandler<SocketBotAddAuditLogData>(ActionType.BotAdded)]
+    private static async Task BotAdded(AuditLogContext<SocketBotAddAuditLogData> ctx)
     {
         var target = await ctx.Data.Target.GetOrDownloadAsync();
-        
-        ctx.Message.WithColor(Color.DarkRed)
+
+        ctx.Message.WithColor(Color.Green)
             .WithTitle("Bot added")
             .WithThumbnailUrl(target.GetDisplayAvatarUrl())
             .AddField("Target", $"{target.Username} ({target.Id})");
