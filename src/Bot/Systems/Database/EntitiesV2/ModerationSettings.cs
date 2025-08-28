442f26a8b278cf079fcf0a62429338da4988446e
GreemDev <greemdev@ryujinx.app>
2025-08-28 14:50:44 -0500

    Reapply "Add the ability to set a secondary mod role"
    This reverts commit 7e37f3815b05f84e5e6d9fc68f897eda7899a31f.

--- src/Bot/Systems/Database/EntitiesV2/ModerationSettings.cs
+++ src/Bot/Systems/Database/EntitiesV2/ModerationSettings.cs
@@ -3,16 +3,18 @@
 public class ModerationSettings
 {
     public ulong ActionLogChannel { get; set; }
-    
+
     public ulong ModRole { get; set; }
-    
+
+    public ulong SecondaryModRole { get; set; }
+
     public ulong AdminRole { get; set; }
-    
+
     public bool CheckAccountAge { get; set; }
-    
+
     public ulong UnverifiedRole { get; set; }
-    
+
     public ulong VerifiedRole { get; set; }
-    
+
     public bool ShowResponsibleModerator { get; set; }
 }
