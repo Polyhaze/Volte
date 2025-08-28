9bc30dba0667e4ee5477237f6470a5bc51309f24
GreemDev <greemdev@ryujinx.app>
2025-08-28 14:50:47 -0500

    Reapply "handle SecondaryModRole in mod precondition for slash commands & make it handle cached stuff without HTTP requests"
    This reverts commit 8e66868b8078ffee92d14b8b4f29685123b2816c.

--- src/Bot/Systems/Commands/Interaction/Attributes/RequireGuildModeratorPreconditionAttribute.cs
+++ src/Bot/Systems/Commands/Interaction/Attributes/RequireGuildModeratorPreconditionAttribute.cs
@@ -17,9 +17,12 @@ public class RequireGuildModeratorPreconditionAttribute : PreconditionAttribute
         var db = services.Get<DatabaseService>();
         var data = await db.GetDataAsync(context.Guild.Id);
 
-        var u = await context.Guild.GetUserAsync(context.User.Id);
+        var u = 
+            context.Guild.Cast<SocketGuild>()?.GetUser(context.User.Id) 
+            ?? await context.Guild.GetUserAsync(context.User.Id);
 
         return u.RoleIds.Contains(data.Settings.Moderation.ModRole) 
+               || u.RoleIds.Contains(data.Settings.Moderation.SecondaryModRole)
                || u.RoleIds.Contains(data.Settings.Moderation.AdminRole) 
                || u.Guild.OwnerId == u.Id
                || u.IsBotOwner()
