7e37f3815b05f84e5e6d9fc68f897eda7899a31f
GreemDev <greemdev@ryujinx.app>
2025-08-28 14:37:12 -0500

    Revert "Add the ability to set a secondary mod role"
    This reverts commit 7edea7ae08fb6e85e54acb5c814474da557dd205.

--- src/Bot/Helpers/DiscordHelper.cs
+++ src/Bot/Helpers/DiscordHelper.cs
@@ -38,7 +38,6 @@ public static class DiscordHelper
 
     public static bool IsModerator(this VolteContext ctx, IGuildUser user)
         => user.HasRole(ctx.GuildData.Settings.Moderation.ModRole) 
-           || user.HasRole(ctx.GuildData.Settings.Moderation.SecondaryModRole)
            || ctx.IsAdmin(user) 
            || IsGuildOwner(user);
 
--- src/Bot/Systems/Commands/Text/Modules/Settings/ModRoleCommand.cs
+++ src/Bot/Systems/Commands/Text/Modules/Settings/ModRoleCommand.cs
@@ -16,21 +16,4 @@ public sealed partial class SettingsModule
         Context.Modify(data => data.Settings.Moderation.ModRole = role.Id);
         return Ok($"Set {role.Mention} as the Moderator role for this guild.");
     }
-    
-    [Command("SecondaryModRole", "ModRole2", "Mod2")]
-    [Description("Sets a secondary role able to use Moderation commands for the current guild.")]
-    public Task<ActionResult> SecondaryModRoleAsync(
-        [Remainder,
-         Description("The role to be set as the secondary Moderator role; or none if you want to reset.")]
-        SocketRole role = null)
-    {
-        if (role is null)
-        {
-            Context.Modify(data => data.Settings.Moderation.SecondaryModRole = 0);
-            return Ok("Reset the secondary Moderator role.");
-        }
-
-        Context.Modify(data => data.Settings.Moderation.ModRole = role.Id);
-        return Ok($"Set {role.Mention} as the secondary Moderator role for this guild.");
-    }
 }
