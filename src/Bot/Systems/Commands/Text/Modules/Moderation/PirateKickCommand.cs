b8fe5b1619a5f733fe993c32c48902a4359f120e
GreemDev <greemdev@ryujinx.app>
2026-03-14 00:59:08 -0500

    feature: (Ryubing guild only) pirate kick command

---
+++ src/Bot/Systems/Commands/Text/Modules/Moderation/PirateKickCommand.cs
@@ -0,0 +1,68 @@
+﻿namespace Volte.Systems.Commands.Text.Modules;
+
+public partial class ModerationModule
+{
+    [Command("PirateKick", "PKick")]
+    [Description(
+        "Kicks the user with a very long-winded message as to why piracy is not supported. Content is retrieved from the 'emulationisnotpiracy' tag.")]
+    [RequireSpecificGuild([1294443224030511104])]
+    public async Task<ActionResult> PirateKickAsync(
+        [CheckHierarchy, EnsureNotSelf, Description("The target member.")]
+        SocketGuildUser user)
+    {
+        bool sentReasonDm = false;
+
+        if (Context.Guild.GetUser(user.Id) is { } member)
+        {
+            var e = Context.CreateEmbedBuilder($"You've been kicked from **{Context.Guild.Name}** for **piracy**.");
+
+            if (!await member.TrySendMessageAsync(embed: e.Apply(Context.GuildData).Build()))
+                Warn(LogSource.Module, $"encountered a 403 when trying to message {member}!");
+
+            var tag = Context.GuildData.GetTagByNameOrAlias("emulationisnotpiracy");
+            if (tag.HasValue)
+            {
+                if (tag.Value.Response.Length <= 2000)
+                {
+                    if (!(sentReasonDm = await member.TrySendMessageAsync(text: tag.Value.Response)))
+                        Warn(LogSource.Module, $"encountered a 403 when trying to message {member}!");
+                }
+                else
+                {
+                    if (!(sentReasonDm =
+                            await member.TrySendMessageAsync(embed: tag.Value.AsContentEmbed(Context).Build())))
+                        Warn(LogSource.Module, $"encountered a 403 when trying to message {member}!");
+                }
+
+                if (sentReasonDm)
+                {
+                    tag.Value.Uses++;
+                    Db.Save(Context.GuildData);
+                }
+            }
+        }
+
+        try
+        {
+            await user.KickAsync(GetReason("Kicked", Context.User, "Piracy"));
+            return Ok(sb =>
+                {
+                    sb.Append($"Successfully kicked **{user}** from this guild.");
+
+                    if (sentReasonDm)
+                        sb.AppendLine()
+                            .Append("They were also DMed a verbose explainer.");
+                },
+                _ => ModerationService.OnModActionCompleteAsync(ModActionEventArgs.InContext(Context)
+                    .WithActionType(ModActionType.Kick)
+                    .WithTarget(user)
+                    .WithReason("Piracy"))
+            );
+        }
+        catch
+        {
+            return BadRequest(
+                "An error occurred kicking that member. Do I have permission; or are they higher than me in the role list?");
+        }
+    }
+}
